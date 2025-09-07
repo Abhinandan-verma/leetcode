ae3a5e564495175fb7c58272b11cb4422fbd1e1e
Abhinandan-verma <abhinandanverma881@gmail.com>
2025-09-07 23:36:55 +0530

    twosum.cpp

---
+++ twosum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> seen;
+        for (int i = 0; i < nums.size(); i++) {
+            int remaining = target - nums[i];
+            if (seen.find(remaining) != seen.end()) {
+                return {seen[remaining], i};
+            }
+            seen[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
